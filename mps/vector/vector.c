404097811880f5140c58736c0cf40b4ecb7b96c5
vyasj <jav_001@hotmail.com>
2025-07-28 22:29:38 -0400

    long time no see

--- mps/vector/vector.c
+++ mps/vector/vector.c
@@ -2,23 +2,42 @@
 #include <stdlib.h>
 
 typedef struct {
-	size_t len;
-	size_t size;
-	void *data;
+	size_t len; // number of elements in the vec
+	size_t size; // size of the vec in bytes
+	size_t cap; // maximum capacity of the vec in bytes
+	int *data; // list of addresses to each element in the vec
 } vector;
 
-vector init_vector() {
-	vector vec;
+void init_vector(vector *vec) {
+	vec->len = 0;
+	vec->size = 0;
+	vec->cap = sizeof(int*);
+	vec->data = malloc(sizeof(int*));
+}
 
-	vec.len = 0;
-	vec.size = 0;
-	vec.data = NULL;
+void free_vector(vector *vec) {
+	free(vec->data);
+}
 
-	return vec;
+void push_vector(vector *vec, void *val) {
+	vec->len++;
+	vec->size = vec->size + sizeof(int);
+
+	if (vec->len > vec->cap) {
+		vec->cap = vec->cap * 2;
+	}
+
+	*(vec->data) = val;
 }
 
 int main() {
-       	vector vec = init_vector();
+	vector *vec;
+	init_vector(vec);
 
+	int *val = 7;
+	push_vector(vec, val);
+
+	free_vector(vec);
+	
 	return 0;
 }
