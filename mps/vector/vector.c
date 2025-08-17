5491617f024b3bb1f68a3fba3cc6d7ab2bcdebb7
vyasj <jav_001@hotmail.com>
2025-08-17 17:51:44 -0400

    progress i think idk im stupid

--- mps/vector/vector.c
+++ mps/vector/vector.c
@@ -1,38 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int INITIAL_CAPACITY;
 
 typedef struct {
 	size_t len;  // number of elements in the vec
-	size_t size; // size of vec in bytes
+	size_t size; // size of each element in bytes
 	size_t cap;  // maximum capacity of the vec in bytes
 	void *data;  // pointer to the array of elements
 } vector;
 
-void init_vec(vector *vec) {
+vector* new_vec(size_t e_size) {
+	vector *vec = malloc(sizeof(vector));
 	vec->len = 0;
-	vec->size = 0;
-	vec->cap = sizeof(void*);
-	vec->data = malloc(sizeof(void*));
+	vec->size = e_size;
+	vec->cap = INITIAL_CAPACITY;
+	vec->data = malloc(INITIAL_CAPACITY * e_size);
+	return vec;
 }
 
 void free_vec(vector *vec) {
 	free(vec->data);
+	free(vec);
 }
 
-void push_vec(vector *vec, void *val) {
-	if (vec->size + sizeof(val) > vec->cap) {
-		vec->cap = 2 * vec->cap;
+void push_vec(vector *vec, const void *e) {
+	if (vec->len + 1 > vec->cap) {
+		vec->cap *= 2;
 		vec->data = realloc(vec->data, vec->cap);
 	}
-
-	vec->size = vec->size + sizeof(val);
-	vec->data[vec->len] = val;
+	void *ptr = vec->data + vec->len;
+	ptr = memcpy(ptr, e, vec->size);
 	vec->len++;
 }
 
-int main() {
-	vector *vec;
-	init_vec(vec);
+void *get_vec(vector *vec, size_t idx) {
+	void *ptr;
+	ptr = memcpy(ptr, vec->data + idx, vec->size);
+	return ptr;
+}
 
+int main() {
+	INITIAL_CAPACITY = 8;
+	vector *vec = new_vec(sizeof(int));
+
+	int val = 7;
+	push_vec(vec, &val);
+
+	int val2 = 10;
+	push_vec(vec, &val2);
+
+	for (int i = 0; i < vec->len; i++) {
+		void *e = get_vec(vec, i);
+		printf("vec->data[%d] = %p\n", i, e);
+	}
+	
 	return 0;
 }
