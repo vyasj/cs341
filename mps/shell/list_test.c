0f5742c0081e746076a64b66430d3e4e620139ac
vyasj <jav_001@hotmail.com>
2025-09-16 22:08:40 -0400

    adding tests for list

---
+++ mps/shell/list_test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "list.h"
+
+int main(int argc, char **argv) {
+
+  list *l = new_list(sizeof(int));
+
+  for (int i = 0; i < 8; i++) {
+    push_list(l, &i);
+  }
+
+  for (int i = 0; i < l->len; i++) {
+    int *val = get_list(l, i);
+    printf("l[%d]: %d\n", i, *val);
+  }
+
+  return 0;
+}
